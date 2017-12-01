09bc7d74d9c752d47357064df97d19386eaac33f
Joe <joe@xxtime.com>
2017-12-01 18:19:40 +0800

    确认交易

--- src/Message/NotificationResponse.php
+++ src/Message/NotificationResponse.php
@@ -3,9 +3,10 @@
 namespace Omnipay\MyCard\Message;
 
 
+use Omnipay\MyCard\Exception\DefaultException;
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\NotificationInterface;
-use Omnipay\MyCard\Exception\DefaultException;
+use Guzzle\Http\Client as HttpClient;
 
 class NotificationResponse extends AbstractResponse implements NotificationInterface
 {
@@ -64,6 +65,25 @@ class NotificationResponse extends AbstractResponse implements NotificationInter
 
     public function confirm()
     {
+        return $this->confirmTransaction();
+    }
+
+
+    // docs: 3.4 確認 MyCard 交易，並進行請款(Server to Server)
+    // 注意: 二次扣款也会失败
+    private function confirmTransaction()
+    {
+        $httpClient = new HttpClient('', array('curl.options' => array(CURLOPT_CONNECTTIMEOUT => 60)));
+        $endpoint = $this->request->getEndpoint('b2b') . '/MyBillingPay/api/PaymentConfirm';
+        $requestData = [
+            'AuthCode' => $this->token
+        ];
+        $response = $httpClient->post($endpoint, null, $requestData)->send();
+        $data = json_decode($response->getBody(), true);
+        if ($data['ReturnCode'] != 1) {
+            throw new DefaultException($data['ReturnMsg']);
+        }
+        return true;
     }
 
 
